c9c62c22d09c0a34559a9026b02381e826252172
birros <birros@protonmail.com>
2023-03-10 01:04:03 +0100

    build: lock -> update & improve code

---
+++ cmd/update/update.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/birros/libmpv-build-wip/pkg/models"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	packages := os.Args[1:]
+	lock, err := newLock(packages...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text, err := marshalLock(lock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(text)
+}
+
+func newLock(packages ...string) (models.Lock, error) {
+	var lock models.Lock = models.Lock{}
+
+	for _, name := range packages {
+		dep, err := newDep(name)
+		if err != nil {
+			return nil, fmt.Errorf("newLock: %w", err)
+		}
+
+		lock[name] = *dep
+	}
+
+	return lock, nil
+}
+
+func newDep(packageName string) (*models.Dep, error) {
+	info, err := brewInfo(packageName)
+	if err != nil {
+		return nil, fmt.Errorf("newDep: %w", err)
+	}
+
+	dep := depFromBrewInfo(info)
+	return dep, nil
+}
+
+func brewInfo(packageName string) (*brewInfoResponse, error) {
+	out, err := exec.Command("brew", "info", "--json", packageName).Output()
+	if err != nil {
+		return nil, fmt.Errorf("brewInfo: %w", err)
+	}
+
+	var resp []brewInfoResponse
+	err = json.Unmarshal(out, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("brewInfo: %w", err)
+	}
+
+	if len(resp) != 1 {
+		return nil, fmt.Errorf("brewInfo: not found")
+	}
+
+	info := resp[0]
+
+	if info.Name != packageName {
+		return nil, fmt.Errorf(
+			"brewInfo: %s: %s != %s", packageName, packageName, info.Name,
+		)
+	}
+
+	return &info, nil
+}
+
+type brewInfoResponse struct {
+	Name     string `json:"name"`
+	Versions struct {
+		Stable string `json:"stable"`
+	} `json:"versions"`
+	Urls struct {
+		Stable struct {
+			URL      string `json:"url"`
+			Checksum string `json:"checksum"`
+		} `json:"stable"`
+	} `json:"urls"`
+}
+
+func depFromBrewInfo(info *brewInfoResponse) *models.Dep {
+	return &models.Dep{
+		Version: info.Versions.Stable,
+		URL:     info.Urls.Stable.URL,
+		Sha256:  info.Urls.Stable.Checksum,
+	}
+}
+
+func marshalLock(lock models.Lock) (string, error) {
+	var buf bytes.Buffer
+	e := yaml.NewEncoder(&buf)
+	e.SetIndent(2)
+
+	e.Encode(lock)
+	err := e.Close()
+	if err != nil {
+		return "", fmt.Errorf("marshalLock: %w", err)
+	}
+
+	text := string(buf.Bytes())
+
+	return text, nil
+}
