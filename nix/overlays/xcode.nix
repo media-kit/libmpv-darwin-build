c5119f0add314cf9eb8d1904c54415040be6ccad
birros <birros@protonmail.com>
2024-12-12 17:52:10 +0100

    feat: bump Xcode version

--- nix/overlays/xcode.nix
+++ nix/overlays/xcode.nix
@@ -1,10 +1,10 @@
 final: prev: {
   darwin = prev.darwin.overrideScope (
     final: prev: {
-      xcode_16_0 = prev.xcode.overrideAttrs (prev: {
-        outputHash = "sha256-4+v7xPRUiNao3ZUNA9otI6I2WfSQtED19Z+MKNvlKC4=";
+      xcode_16_1 = prev.xcode.overrideAttrs (prev: {
+        outputHash = "sha256-1jyRJVyOmGA7fxRwBnxSJatnOFDu01RJ9aAQXJNuWBw=";
       });
-      xcode = final.xcode_16_0;
+      xcode = final.xcode_16_1;
     }
   );
 }
